601ec537df2a6ce69d776bca6ad581509fd6f2fd
gomesht <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>
2025-08-21 08:33:09 -0300

    Mudança de tema

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -9,3 +9,24 @@ document.querySelectorAll('nav a').forEach(link => {
  
   });
 });
+
+document.querySelectorAll('[id^="ep"]').forEach(ep => {
+  ep.addEventListener('click', function() {
+    const detalhes = this.parentElement.querySelector('.detalhes');
+    const isOpen = detalhes.style.display === 'flex';
+
+
+    document.querySelectorAll('[id^="ep"]').forEach(div => {
+      div.classList.remove('ep-aberto');
+    });
+   
+    document.querySelectorAll('.detalhes').forEach(div => {
+      div.style.display = 'none';
+    });
+
+    if (!isOpen) {
+      detalhes.style.display = 'flex';
+      this.classList.add('ep-aberto');
+    }
+  });
+});
